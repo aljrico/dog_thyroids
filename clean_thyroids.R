a3deb20e34379dc39f3c0838590f09b30faa34f0
aljrico <alejandro.rico@supersolid.com>
2019-08-20 11:07:47 +0100

    unified left and right

--- clean_thyroids.R
+++ clean_thyroids.R
@@ -8,7 +8,7 @@ library(harrypotter)
 
 library(ggpubr)
 
-get_clean_thyroids <- function(){
+get_clean_thyroids <- function(unify_left_right = TRUE){
   
   df <- 
     read_excel('thyroids_values.xlsx') %>% 
@@ -83,78 +83,147 @@ get_clean_thyroids <- function(){
     select(-`right NC`, -`Left NC`, -`right WC`, -`Left WC`) %>% 
     data.table()
   
+  if(unify_left_right){
+    df <-
+      df %>% 
+      melt(id.vars = c('id', 'name', 'race', 'f')) %>% 
+      filter(! (variable %>% str_detect('_sd'))) %>% 
+      mutate(variable = variable %>% str_remove_all('left_') %>% str_remove_all('right_') %>% str_remove_all('_avg')) %>% 
+      rename(contrast = variable) %>% 
+      data.table()
+  }
+  
   return(df)
 }
-thyroids_unique_value <- function(){
-  clean_thyroids %>% 
-    group_by(id) %>% 
-    summarise(
-      right_NC = mean(right_NC_avg, na.rm = TRUE),
-      left_NC = mean(left_NC_avg, na.rm = TRUE),
-      right_WC = mean(right_WC_avg, na.rm = TRUE),
-      left_WC = mean(left_WC_avg, na.rm = TRUE)
-    ) %>% 
-    inner_join(
-      read_excel('datos_1.xlsx') %>% 
-        rename(id = `Nº (P)`) %>% 
-        select(id, atenuacion_baja, braquicefalico) %>% 
-        na.omit()
-    ) %>% 
-    as_tibble()
+thyroids_unique_value <- function(unified_left_right = TRUE){
+  
+  if(unified_left_right){
+    clean_thyroids %>% 
+      select(id, contrast, value) %>% 
+      group_by(id, contrast) %>% 
+      summarise(value = value %>% mean(na.rm = TRUE))
+  }else{
+    clean_thyroids %>% 
+      group_by(id) %>% 
+      summarise(
+        right_NC = mean(right_NC_avg, na.rm = TRUE),
+        left_NC = mean(left_NC_avg, na.rm = TRUE),
+        right_WC = mean(right_WC_avg, na.rm = TRUE),
+        left_WC = mean(left_WC_avg, na.rm = TRUE)
+      ) %>% 
+      inner_join(
+        read_excel('datos_1.xlsx') %>% 
+          rename(id = `Nº (P)`) %>% 
+          select(id, atenuacion_baja, braquicefalico) %>% 
+          na.omit()
+      ) %>% 
+      as_tibble()
+  }
+
 }
-braqui_atenuation_test <- function(){
-  clean_thyroids %>% 
-    group_by(id) %>% 
-    summarise(
-      right_NC = mean(right_NC_avg, na.rm = TRUE),
-      left_NC = mean(left_NC_avg, na.rm = TRUE),
-      right_WC = mean(right_WC_avg, na.rm = TRUE),
-      left_WC = mean(left_WC_avg, na.rm = TRUE)
-    ) %>% 
-    inner_join(
-      read_excel('datos_1.xlsx') %>% 
-        rename(id = `Nº (P)`) %>% 
-        select(id, atenuacion_baja, braquicefalico) %>% 
-        na.omit()
-    ) %>% 
-    as_tibble() %>% 
-    melt(id.vars = c('id', 'atenuacion_baja', 'braquicefalico')) %>% 
-    ggplot(aes(x = braquicefalico, y = value, fill = braquicefalico)) +
-    geom_boxplot(size = 0.5, outlier.shape = NA) +
-    facet_wrap(variable~., scales = 'free') +
-    stat_compare_means(method = 't.test') +
-    scale_x_discrete(labels = c('No Braqui', 'Braqui')) +
-    scale_fill_got_d(option = 'Margaery', labels = c('No Braquicefálico', 'Braquicefálico'), name = '', guide = FALSE) +
-    theme_pubr() +
-    xlab('') +
-    ylab('Atenuation Value')
+braqui_atenuation_test <- function(unified_left_right = TRUE){
+  
+  if(unified_left_right){
+    clean_thyroids %>% 
+      group_by(id, contrast) %>% 
+      summarise(value = mean(value, na.rm = TRUE)) %>% 
+      inner_join(
+        read_excel('datos_1.xlsx') %>% 
+          rename(id = `Nº (P)`) %>% 
+          select(id, atenuacion_baja, braquicefalico) %>% 
+          na.omit()
+      ) %>% 
+      as_tibble() %>% 
+      ggplot(aes(x = braquicefalico, y = value, fill = braquicefalico)) +
+      geom_boxplot(size = 0.5, outlier.shape = NA) +
+      facet_wrap(contrast~., scales = 'free') +
+      stat_compare_means(method = 't.test') +
+      scale_x_discrete(labels = c('No Braqui', 'Braqui')) +
+      scale_fill_got_d(option = 'Margaery', labels = c('No Braquicefálico', 'Braquicefálico'), name = '', guide = FALSE) +
+      theme_pubr() +
+      xlab('') +
+      ylab('Atenuation Value')
+  }else{
+    
+    clean_thyroids %>% 
+      group_by(id) %>% 
+      summarise(
+        right_NC = mean(right_NC_avg, na.rm = TRUE),
+        left_NC = mean(left_NC_avg, na.rm = TRUE),
+        right_WC = mean(right_WC_avg, na.rm = TRUE),
+        left_WC = mean(left_WC_avg, na.rm = TRUE)
+      ) %>% 
+      inner_join(
+        read_excel('datos_1.xlsx') %>% 
+          rename(id = `Nº (P)`) %>% 
+          select(id, atenuacion_baja, braquicefalico) %>% 
+          na.omit()
+      ) %>% 
+      as_tibble() %>% 
+      melt(id.vars = c('id', 'atenuacion_baja', 'braquicefalico')) %>% 
+      ggplot(aes(x = braquicefalico, y = value, fill = braquicefalico)) +
+      geom_boxplot(size = 0.5, outlier.shape = NA) +
+      facet_wrap(variable~., scales = 'free') +
+      stat_compare_means(method = 't.test') +
+      scale_x_discrete(labels = c('No Braqui', 'Braqui')) +
+      scale_fill_got_d(option = 'Margaery', labels = c('No Braquicefálico', 'Braquicefálico'), name = '', guide = FALSE) +
+      theme_pubr() +
+      xlab('') +
+      ylab('Atenuation Value')
+  }
+ 
 }
-sick_thyroids_test <- function(){
-  clean_thyroids %>% 
-    group_by(id) %>% 
-    summarise(
-      right_NC = mean(right_NC_avg, na.rm = TRUE),
-      left_NC = mean(left_NC_avg, na.rm = TRUE),
-      right_WC = mean(right_WC_avg, na.rm = TRUE),
-      left_WC = mean(left_WC_avg, na.rm = TRUE)
-    ) %>% 
-    inner_join(
-      read_excel('datos_1.xlsx') %>% 
-        rename(id = `Nº (P)`) %>% 
-        select(id, atenuacion_baja, control) %>% 
-        na.omit()
-    ) %>% 
-    as_tibble() %>% 
-    melt(id.vars = c('id', 'atenuacion_baja', 'control')) %>% 
-    ggplot(aes(x = control, y = value, fill = control)) +
-    geom_boxplot(size = 0.5, outlier.shape = NA) +
-    facet_wrap(variable~., scales = 'free') +
-    stat_compare_means(method = 't.test') +
-    scale_x_discrete() +
-    scale_fill_got_d(option = 'Margaery', name = '', guide = FALSE) +
-    theme_pubr() +
-    xlab('') +
-    ylab('Atenuation Value')
+sick_thyroids_test <- function(unified_left_right = TRUE){
+  if(unified_left_right){
+    clean_thyroids %>% 
+      group_by(id, contrast) %>% 
+      summarise(value = mean(value, na.rm = TRUE)) %>% 
+      inner_join(
+        read_excel('datos_1.xlsx') %>% 
+          rename(id = `Nº (P)`) %>% 
+          select(id, atenuacion_baja, control) %>% 
+          na.omit()
+      ) %>% 
+      as_tibble() %>% 
+      ggplot(aes(x = control, y = value, fill = control)) +
+      geom_boxplot(size = 0.5, outlier.shape = NA) +
+      facet_wrap(contrast~., scales = 'free') +
+      stat_compare_means(method = 't.test') +
+      scale_x_discrete() +
+      scale_fill_got_d(option = 'Margaery', name = '', guide = FALSE) +
+      theme_pubr() +
+      xlab('') +
+      ylab('Atenuation Value')
+  }else{
+    
+    
+    clean_thyroids %>% 
+      group_by(id) %>% 
+      summarise(
+        right_NC = mean(right_NC_avg, na.rm = TRUE),
+        left_NC = mean(left_NC_avg, na.rm = TRUE),
+        right_WC = mean(right_WC_avg, na.rm = TRUE),
+        left_WC = mean(left_WC_avg, na.rm = TRUE)
+      ) %>% 
+      inner_join(
+        read_excel('datos_1.xlsx') %>% 
+          rename(id = `Nº (P)`) %>% 
+          select(id, atenuacion_baja, control) %>% 
+          na.omit()
+      ) %>% 
+      as_tibble() %>% 
+      melt(id.vars = c('id', 'atenuacion_baja', 'control')) %>% 
+      ggplot(aes(x = control, y = value, fill = control)) +
+      geom_boxplot(size = 0.5, outlier.shape = NA) +
+      facet_wrap(variable~., scales = 'free') +
+      stat_compare_means(method = 't.test') +
+      scale_x_discrete() +
+      scale_fill_got_d(option = 'Margaery', name = '', guide = FALSE) +
+      theme_pubr() +
+      xlab('') +
+      ylab('Atenuation Value')
+  }
+
 }
 
 sample_size <- function(){
@@ -216,52 +285,94 @@ sample_size <- function(){
 }
 
 
-clean_thyroids <- get_clean_thyroids()
-
-blood_thyroids_corr <- function(){
+clean_thyroids <- get_clean_thyroids(unify_left_right = TRUE)
+blood_thyroids_corr <- function(unified_left_right = TRUE){
   
-  dat <- 
-    read_excel('datos_1.xlsx', sheet = 'Full2') %>% 
-    rename(T4 = `T4 (ug/dL)`,
-           TSH = `TSH (ng/mL)`) %>% 
-    select(Nom, T4, TSH) %>% 
-    mutate(
-      T4 = T4 %>% str_remove_all('\\<') %>% as.numeric(),
-      TSH = TSH %>% str_remove_all('\\<') %>% as.numeric()
-    ) %>% 
-    na.omit() %>% 
-    mutate(name = Nom %>% tolower()) %>% 
-    select(-Nom) %>% 
-    inner_join(
-      clean_thyroids %>% 
-        group_by(name) %>% 
-        summarise(
-          right_NC = mean(right_NC_avg, na.rm = TRUE),
-          left_NC = mean(left_NC_avg, na.rm = TRUE),
-          right_WC = mean(right_WC_avg, na.rm = TRUE),
-          left_WC = mean(left_WC_avg, na.rm = TRUE)
-        ) %>% 
-        mutate(name = name %>% tolower())
-    ) %>% 
-    melt(id.vars = c('T4', 'TSH', 'name')) 
-  
-  
-  setDT(dat)
-  
-  sum_t4 <- dat[,summary(lm(T4~value))$r.squared,by=variable] %>% rename(r_squared = V1)
-  sum_tsh <- dat[,summary(lm(TSH~value))$r.squared,by=variable] %>% rename(r_squared = V1)
-  
-  t4_plot <- dat %>% 
-    ggplot(aes( x = value, y = T4, colour = variable)) +
-    geom_point() +
-    geom_smooth(method = 'lm', se = FALSE) +
-    facet_wrap(variable~., scales = 'free')
-  
-  tsh_plot <- dat %>% 
-    ggplot(aes( x = value, y = TSH, colour = variable)) +
-    geom_point() +
-    geom_smooth(method = 'lm', se = FALSE) +
-    facet_wrap(variable~., scales = 'free')
+  if(unified_left_right){
+    dat <- 
+      read_excel('datos_1.xlsx', sheet = 'Full2') %>% 
+      rename(T4 = `T4 (ug/dL)`,
+             TSH = `TSH (ng/mL)`) %>% 
+      select(Nom, T4, TSH) %>% 
+      mutate(
+        T4 = T4 %>% str_remove_all('\\<') %>% as.numeric(),
+        TSH = TSH %>% str_remove_all('\\<') %>% as.numeric()
+      ) %>% 
+      na.omit() %>% 
+      mutate(name = Nom %>% tolower()) %>% 
+      select(-Nom) %>% 
+      inner_join(
+        clean_thyroids %>% 
+          group_by(name, contrast) %>% 
+          summarise(value = mean(value, na.rm = TRUE)) %>% 
+          ungroup() %>% 
+          mutate(name = name %>% tolower())
+      ) %>% 
+      data.table()
+    
+    
+    setDT(dat)
+    
+    sum_t4 <- dat[,summary(lm(T4~value))$r.squared,by=contrast] %>% rename(r_squared = V1)
+    sum_tsh <- dat[,summary(lm(TSH~value))$r.squared,by=contrast] %>% rename(r_squared = V1)
+    
+    t4_plot <- dat %>% 
+      ggplot(aes( x = value, y = T4, colour = contrast)) +
+      geom_point() +
+      geom_smooth(method = 'lm', se = FALSE) +
+      facet_wrap(contrast~., scales = 'free')
+    
+    tsh_plot <- dat %>% 
+      ggplot(aes( x = value, y = TSH, colour = contrast)) +
+      geom_point() +
+      geom_smooth(method = 'lm', se = FALSE) +
+      facet_wrap(contrast~., scales = 'free')
+    
+  }else{
+    
+    dat <- 
+      read_excel('datos_1.xlsx', sheet = 'Full2') %>% 
+      rename(T4 = `T4 (ug/dL)`,
+             TSH = `TSH (ng/mL)`) %>% 
+      select(Nom, T4, TSH) %>% 
+      mutate(
+        T4 = T4 %>% str_remove_all('\\<') %>% as.numeric(),
+        TSH = TSH %>% str_remove_all('\\<') %>% as.numeric()
+      ) %>% 
+      na.omit() %>% 
+      mutate(name = Nom %>% tolower()) %>% 
+      select(-Nom) %>% 
+      inner_join(
+        clean_thyroids %>% 
+          group_by(name) %>% 
+          summarise(
+            right_NC = mean(right_NC_avg, na.rm = TRUE),
+            left_NC = mean(left_NC_avg, na.rm = TRUE),
+            right_WC = mean(right_WC_avg, na.rm = TRUE),
+            left_WC = mean(left_WC_avg, na.rm = TRUE)
+          ) %>% 
+          mutate(name = name %>% tolower())
+      ) %>% 
+      melt(id.vars = c('T4', 'TSH', 'name')) 
+    
+    
+    setDT(dat)
+    
+    sum_t4 <- dat[,summary(lm(T4~value))$r.squared,by=variable] %>% rename(r_squared = V1)
+    sum_tsh <- dat[,summary(lm(TSH~value))$r.squared,by=variable] %>% rename(r_squared = V1)
+    
+    t4_plot <- dat %>% 
+      ggplot(aes( x = value, y = T4, colour = variable)) +
+      geom_point() +
+      geom_smooth(method = 'lm', se = FALSE) +
+      facet_wrap(variable~., scales = 'free')
+    
+    tsh_plot <- dat %>% 
+      ggplot(aes( x = value, y = TSH, colour = variable)) +
+      geom_point() +
+      geom_smooth(method = 'lm', se = FALSE) +
+      facet_wrap(variable~., scales = 'free')
+  }
   
   return(list(t4_plot = t4_plot, tsh_plot = tsh_plot))
   
